e17ce13644831d948463a0d193dfaf07fc92bb33
roverchen <rover.k.chen@gmail.com>
2021-08-23 23:17:54 +0800

    webView

---
+++ MainActivity.cs
@@ -0,0 +1,51 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Webkit;
+
+namespace WorkingWithWebview.Android
+{
+    [Activity(Label = "WorkingWithWebview.Android.Android", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        WebView web_view;
+
+        public class HelloWebViewClient : WebViewClient
+        {
+            [System.Obsolete]
+            public override bool ShouldOverrideUrlLoading(WebView view, string url)
+            {
+                view.LoadUrl(url);
+                return false;
+            }
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            web_view = FindViewById<WebView>(Resource.Id.webview);
+            web_view.Settings.JavaScriptEnabled = true;
+            web_view.SetWebViewClient(new HelloWebViewClient());
+            web_view.LoadUrl("https://pig-2beaa9.ingress-earth.easywp.com/app");
+            /*
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            Xamarin.Essentials.Platform.Init(this, bundle);
+
+            LoadApplication(new App());
+            */
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ obj/Debug/android/src/crc6451832da552dca153/MainActivity_HelloWebViewClient.java
@@ -0,0 +1,47 @@
+package crc6451832da552dca153;
+
+
+public class MainActivity_HelloWebViewClient
+	extends android.webkit.WebViewClient
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_shouldOverrideUrlLoading:(Landroid/webkit/WebView;Ljava/lang/String;)Z:GetShouldOverrideUrlLoading_Landroid_webkit_WebView_Ljava_lang_String_Handler\n" +
+			"";
+		mono.android.Runtime.register ("WorkingWithWebview.Android.MainActivity+HelloWebViewClient, WorkingWithWebview.Android", MainActivity_HelloWebViewClient.class, __md_methods);
+	}
+
+
+	public MainActivity_HelloWebViewClient ()
+	{
+		super ();
+		if (getClass () == MainActivity_HelloWebViewClient.class)
+			mono.android.TypeManager.Activate ("WorkingWithWebview.Android.MainActivity+HelloWebViewClient, WorkingWithWebview.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public boolean shouldOverrideUrlLoading (android.webkit.WebView p0, java.lang.String p1)
+	{
+		return n_shouldOverrideUrlLoading (p0, p1);
+	}
+
+	private native boolean n_shouldOverrideUrlLoading (android.webkit.WebView p0, java.lang.String p1);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
